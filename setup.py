27a1ecf7035c4802dc34404a176e2dacd08d833c
Alper Sakarya <alpersakarya@gmail.com>
2016-05-04 19:35:37 -0400

    Few changes

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'myls',
-  packages = ['myls'], 
-  version = '0.1',
-  description = 'A that lets you view file and directory access rights with octal',
-  author = 'Alper Sakarya',
-  author_email = 'alpersakarya@gmail.com',
-  url = 'https://github.com/AlperSakarya/myls', 
-  download_url = 'https://github.com/AlperSakarya/myls/tarball/0.1', 
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
+    name='myls',
+    packages=['myls'],
+    version='0.1',
+    description='A that lets you view file and directory access rights with octal',
+    uthor='Alper Sakarya',
+    author_email='alpersakarya@gmail.com',
+    url='https://github.com/AlperSakarya/myls',
+    download_url='https://github.com/AlperSakarya/myls/tarball/0.1',
+    keywords=['testing', 'logging', 'example'],
+    classifiers = [],
+    scripts=['/usr/bin/local/myls']
 )
