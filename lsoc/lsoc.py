53643f8d2b4bf6945b7fed4ffa203131a8600b1e
Alper Sakarya <alpersakarya@gmail.com>
2023-11-17 19:12:20 -0500

    moved the OS out

--- lsoc/lsoc.py
+++ lsoc/lsoc.py
@@ -2,12 +2,14 @@
 import os
 import sys
 
-
 def lsoccmd():
-    if not len(sys.argv) >1:
-        lsoc = 'stat -c "%a %n" *'
-        os.system(lsoc)
-    else:
+    if len(sys.argv) > 1:
         myFile = str(sys.argv[1])
         lsoc = 'stat -c "%a %n" {0}'.format(myFile)
-        os.system(lsoc)
+    else:
+        lsoc = 'stat -c "%a %n" *'
+
+    os.system(lsoc)
+
+# Call the function
+lsoccmd()
