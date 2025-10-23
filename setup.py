457af1e9ea39aa6b1e16b8a7e2c9a034706dfc74
AlperSakarya <alpersakarya@gmail.com>
2025-10-22 21:04:06 -0400

    Update setup.py to include README.md as long description for PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='lsoc',
     packages=['lsoc'],
-    version='0.1.3',
+    version='0.1.4',
     description='Lite Python package that lets you view file and directory access rights with octal',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Alper Sakarya',
     author_email='alpersakarya@gmail.com',
     url='https://github.com/AlperSakarya/lsoc',
